8935f12a14a5407f3e4cc69be6610da2bc0e2a08
Siddhu Yeturi <siddhuyeturi@gmail.com>
2021-08-18 01:57:19 +0000

    second commit

--- create_iso.sh
+++ create_iso.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
-# Install archiso
-[[ $(pacman -Qs archiso) ]] || pacman -S archiso
-# Copy the archiso profile relang to archiso
-cp -r /usr/share/archiso/configs/releng archiso
-
-# check if necessary directories exist if not create them
-[ -d "build" ] || mkdir build
-[ -d "tmp" ] || mkdir tmp
-
+init(){
+    # check if necessary directories exist if not create them
+    [ -d "build" ] || mkdir build
+    [ -d "tmp" ] || mkdir tmp
+    [[ $(pacman -Qs archiso) ]] || sudo pacman -S archiso
+    cp -r /usr/share/archiso/configs/releng archiso
+    yes | cp -r src/* archiso/
+    #for i in $(ls src)
+    #do
+    #    echo $i
+    #    rm -r archiso/$i
+    #    cp -r src/$i archiso/
+    #done
+}
 # check if tmp directory is empty
 # if not delete everything in it
 # finally run the mkarchiso command
-[ "$(ls -A tmp)" ] &&  rm -r tmp/*
-mkarchiso -v -w tmp -o build archiso 2>&1
+build(){
+    [ "$(ls -A tmp)" ] &&  rm -r tmp/*
+    sudo mkarchiso -v -w tmp -o build archiso 2>&1
+}
 
+case $1 in
+    "init") init;;
+    "build") build;;
+esac
